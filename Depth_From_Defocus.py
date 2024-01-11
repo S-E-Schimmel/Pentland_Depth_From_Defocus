69565de7e59ea6171ff88de497bad7a20b32d7a6
S-E-Schimmel <s.e.schimmel@outlook.com>
2024-01-11 15:47:43 +0100

    Update

--- Depth_From_Defocus.py
+++ Depth_From_Defocus.py
@@ -1,6 +1,8 @@
-#S.E. Schimmel
-#Depth_From_Defocus
-#03-12-2023
+"""
+S.E. Schimmel - S.E.Schimmel@student.tudelft.nl
+Bachelor Final Project - WB3BEP-16 - TU Delft 2023-2024
+Depth from Defocus using a sharp and blurred image of the same scene
+"""
 
 import Functions
 import cv2
@@ -8,36 +10,40 @@ import time
 
 start_time = time.time()
 
-# Load images
+# Load the cropped image patches
 sharp_image = cv2.imread(r"S80CC.png")
 blurred_image = cv2.imread(r'B80CC.png')
 
-# Convert images to grayscale
+# Convert the image patches to grayscale
 gray_sharp, gray_blurred = Functions.convert_to_grayscale(sharp_image,blurred_image)
 
-# Compute the Discrete Fourier Transform of both images, Shift the zero-frequency component to the center of the spectrum, Calculate the magnitude of the Fourier Transform, Scale the magnitude for display, Calculate difference (natural log) of images
+# Compute the Discrete Fourier Transform of both images
+# Shift the zero-frequency component to the center of the spectrum
+# Calculate the magnitude of the Fourier Transform
+# Scale the magnitude for display
+# Calculate difference (natural log) of images
 ln_difference, scaled_magnitude_image1, scaled_magnitude_image2 = Functions.DFT_Shifted_Scaled(gray_sharp,gray_blurred)
 
 # Calculate average of max ln difference for top 10 rows and first 10 columns
 average = Functions.Calculate_Average(ln_difference,10)
 
-# Select suitable high frequencies
+# Select suitable high spatial frequencies
 selected_indices = Functions.Select_Indices(ln_difference,average,10)
 
 # Calculate Depth by taking average of multiple depth estimations
 Average_Depth = Functions.Calculate_Depth(ln_difference,selected_indices)
 
+#Print Calculated Depth
 print('Calculated Depth:', Average_Depth,'mm')
 
+#Print Runtime
 print("--- %s seconds ---" % (time.time() - start_time))
 
-true_depth = 1380 #mm
-print('True Depth: ', true_depth,"mm")
+#true_depth = 1380 #mm
+#print('True Depth: ', true_depth,"mm")
 
-# Display the magnitude of the Fourier Transform
+# Display the magnitude of the Fourier Transform (with zero-frequency shifted to the center)
 cv2.imshow('Fourier Transform Image 1', scaled_magnitude_image1)
 cv2.imshow('Fourier Transform Image 2', scaled_magnitude_image2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
--- Functions.py
+++ Functions.py
@@ -1,19 +1,24 @@
-#S.E. Schimmel
-#03-12-2023
+"""
+S.E. Schimmel - S.E.Schimmel@student.tudelft.nl
+Bachelor Final Project - WB3BEP-16 - TU Delft 2023-2024
+Depth from Defocus using a sharp and blurred image of the same scene
+"""
 
 import cv2
 import numpy as np
 
 # Known camera intrinsic values
-F=55 #mm
+F=55 #mm (= Focal Length of Camera Lens)
 v0=59.06 #mm (Focus Distance = 80cm)
-f1=5.6
+f1=5.6 # (= f-number at which the lens aperture is set)
 
+# Convert image to grayscale image (also changes image shape from height, width, channel to height, width only)
 def convert_to_grayscale(sharp_image,blurred_image):
     gray_sharp = cv2.cvtColor(sharp_image, cv2.COLOR_BGR2GRAY)
     gray_blurred = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2GRAY)
     return(gray_sharp,gray_blurred)
 
+# Compute the Discrete Fourier Tranform, Shift the zero-frequency component, Compute the magnitude and Scale the magnitude for display
 def DFT_Shifted_Scaled(gray_sharp,gray_blurred):
     # Compute the discrete Fourier Transform of the images
     fourier_sharp = cv2.dft(np.float32(gray_sharp), flags=cv2.DFT_COMPLEX_OUTPUT)
@@ -38,42 +43,43 @@ def DFT_Shifted_Scaled(gray_sharp,gray_blurred):
 
     return(ln_difference,scaled_magnitude_image1,scaled_magnitude_image2)
 
-def Calculate_Depth(ln_difference,selected_indices):
-    depth_sum=0
-    for i in range(len(selected_indices)):
-        u_array=selected_indices[i][0]
-        v_array=selected_indices[i][1]
-        u=np.abs(u_array-82)/164
-        v=np.abs(v_array-91)/182
-        bottom = 2*np.pi**2*((u**2)+(v**2))
-        sigma1 = np.sqrt((ln_difference[u_array][v_array]) / (bottom))
-        depth = F * v0 / (v0 - F - sigma1 * f1)
-        depth_sum+=depth
-    Average_Depth = depth_sum/len(selected_indices)
-    return(Average_Depth)
-
+# Calculate the average of the maximum values in terms of ln_difference from the top 10 rows and most left 10 columns
 def Calculate_Average(ln_difference,n):
     sum = 0
-
     for x in range(n):
         sum += np.max(ln_difference[x])
         sum += np.max(ln_difference[:, x])
-
     average = sum / (2 * n)
     return(average)
 
+#  Save the indices from suitable frequencies
 def Select_Indices(ln_difference,average,n):
-    selected_indices = []
-    for y in range(n):
-        ind = np.argpartition(ln_difference[y], -5)[-5:]
-        ind2 = np.argpartition(ln_difference[:, y], -5)[-5:]
-        for i in range(5):
-            if np.abs(ln_difference[y][ind][i] - average) < 0.2:
-                # print("Suitable LN Difference:",ln_difference[y][ind][i])
-                tuple = (y, ind[i])
-                selected_indices.append(tuple)
-                # print("Tuple to be added to selected_indices:", tuple)
-            if np.abs(ln_difference[:, y][ind2][i] - average) < 0.2:
-                tuple = (ind2[i], y)
-                selected_indices.append(tuple)
+    selected_indices = [] # Create an empty list to store the left and right index of the blur circle into
+    for y in range(n): # Execute for the first 10 column and top 10 rows
+        ind = np.argpartition(ln_difference[y], -5)[-5:] # Find top 5 highest ln-differences per row
+        ind2 = np.argpartition(ln_difference[:, y], -5)[-5:] # Find top 5 highest ln-differences per column
+        for i in range(5): # Go through the top 5 highest ln-differences
+            if np.abs(ln_difference[y][ind][i] - average) < 0.2: # Only execute if close to the calculated average
+                tuple = (y, ind[i]) # Create a tuple using the indices
+                selected_indices.append(tuple) # Store the tuple in the list
+            if np.abs(ln_difference[:, y][ind2][i] - average) < 0.2: # Only execute if close to the calculated average
+                tuple = (ind2[i], y) # Create a tuple using the indices
+                selected_indices.append(tuple) # Store the tuple in the list
     return selected_indices
+
+# Estimate the Depth
+def Calculate_Depth(ln_difference,selected_indices):
+    depth_sum=0 # Initialize counter variable at 0
+    for i in range(len(selected_indices)):
+        u_array=selected_indices[i][0] # Grab index from list
+        v_array=selected_indices[i][1] # Grab index from list
+        u=np.abs(u_array-82)/164 # Determine u frequency
+        v=np.abs(v_array-91)/182 # Determine v frequency
+        denominator = 2*np.pi**2*((u**2)+(v**2)) # Calculate denominator
+        sigma1 = np.sqrt((ln_difference[u_array][v_array]) / (denominator)) # Calculate radius of blur circle
+        depth = F * v0 / (v0 - F - sigma1 * f1) # Calculate Depth
+        depth_sum+=depth # Add 1 to counter variable
+    Average_Depth = depth_sum/len(selected_indices) # Determine final depth estimation
+    return(Average_Depth)
+
+
